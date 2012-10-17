087aebc3592e8cee17c08bfd216ffcd5b4ab7c7c
Jari Aalto <jari.aalto@cante.net>
2012-10-17 12:12:56 +0300

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/undo-tree-mode-epackage-install.el
@@ -0,0 +1,4 @@
+(autoload 'undo-tree-mode                       "undo-tree" "" t)
+(autoload 'turn-on-undo-tree-mode               "undo-tree" "" t)
+(autoload 'global-undo-tree-mode                "undo-tree" "" t)
+(provide 'undo-tree-mode-epackage-install)
---
+++ epackage/undo-tree-mode-epackage-uninstall.el
@@ -0,0 +1,2 @@
+;; Template. Add remove-hook etc calls here.
+(error "undo-tree-mode-epackage-uninstall.el is not a configuration file.")
